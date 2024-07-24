f19c2f16ff8b8015d81e308c29e70c8012100cc3
Deepak A <78550548+Anbudeepak@users.noreply.github.com>
2024-07-24 15:46:40 +0530

    adding Num.of calls and duration file

--- Number of calls and total duration.py
+++ Number of calls and total duration.py
@@ -1,6 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,when,count
+from pyspark.sql.functions import col,when,count,sum
 spark = SparkSession.builder.appName('demo3').getOrCreate()
 data = [(10,20,58),
         (20,10,12),
@@ -17,3 +17,4 @@ df_classify = df.withColumn("person1",when(col("from_id")<col("to_id"),col("from
 df_final = (df_classify.groupBy(col('person1'),col('person2'))
             .agg(sum(col("duration")).alias("total_duration"),count(col('person1')).alias("call_count"))
             .select('person1','person2','call_count','total_duration'))
+df_final.display()
